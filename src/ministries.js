fcf52dc29400ad985a25db92c42c566b9953b377
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2023-11-22 10:22:42 -0800

    img fix

--- src/ministries.js
+++ src/ministries.js
@@ -16,6 +16,7 @@ const Ministries = () => {
     const [active, setActive] = useState(false);
 
     const handleButtonClick = (index) => {
+        // window.location.href="/ministryDetail"
         setActive(index);
     };
 
@@ -26,12 +27,12 @@ const Ministries = () => {
     }, [])
 
     const cards = [
-        {id: '1', title : 'Prayer Meetings', description: 'Lorem Ipsum dolor sit amet....', img: {prayer}},
-        {id: '2', title : 'Bible Study', description: 'Lorem Ipsum dolor sit amet....', img: {bibleStudy}},
-        {id: '3', title : "Men's Meeting", description: 'Lorem Ipsum dolor sit amet....', img: {menMeet}},
-        {id: '4', title : "Women's Meeting", description: 'Lorem Ipsum dolor sit amet....', img: {womenMeet}},
-        {id: '5', title : "Senior's Meeting", description: 'Lorem Ipsum dolor sit amet....', img: {seniorMeet}},
-        {id: '6', title : "Children's Secondary School", description: 'Lorem Ipsum dolor sit amet....', img: {childMeet}},
+        {id: '1', title : 'Prayer Meetings', description: 'Lorem Ipsum dolor sit amet....', img: prayer},
+        {id: '2', title : 'Bible Study', description: 'Lorem Ipsum dolor sit amet....', img: bibleStudy},
+        {id: '3', title : "Men's Meeting", description: 'Lorem Ipsum dolor sit amet....', img: menMeet},
+        {id: '4', title : "Women's Meeting", description: 'Lorem Ipsum dolor sit amet....', img: womenMeet},
+        {id: '5', title : "Senior's Meeting", description: 'Lorem Ipsum dolor sit amet....', img: seniorMeet},
+        {id: '6', title : "Children's Secondary School", description: 'Lorem Ipsum dolor sit amet....', img: childMeet},
     ]
 
     return (
